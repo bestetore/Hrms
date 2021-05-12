e623ac505eb8ed54fdb2b7e2212b76d01868ff2c
bestetore <76864441+bestetore@users.noreply.github.com>
2021-05-13 00:49:06 +0300

    Hrms

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,53 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="jobPositions")
+public class JobPosition {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="position")
+	private String position;
+	
+	public JobPosition() {
+		
+	}
+	
+	public JobPosition(int id, String position) {
+		super();
+		this.id = id;
+		this.position = position;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getPosition() {
+		return position;
+	}
+
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+}
